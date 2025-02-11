6f609f230cafe2ed176ec34587db9979204b4e9d
Arath Campos <mi.familia.55555@gmail.com>
2025-02-11 13:26:13 -0600

    Creating clearScreen func and generating requested random number

--- C/guess number/main.c
+++ C/guess number/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <conio.h>
+#include <time.h>
+#define MIN_RAND_RANGE 1
+#define MAX_RAND_RANGE 100
 
-int main () {
+// cleans screen by using ANSI scape codes
+void clearScreen() {
+  printf("\033[H\033[J");
+  return;
+}
 
+int main()
+{
+  clearScreen(); // Ensures a clean screen
+  srand(time(NULL)); // Time seed for random number creation
+
+  // Generates a random number between MIN_RAND_RANGE and MAX_RAND_RANGE
+  const unsigned short int randNum
+    = rand() % (MAX_RAND_RANGE - MIN_RAND_RANGE + 1) + MIN_RAND_RANGE;
+  bool isCorrect = false;
   return 0;
 }
