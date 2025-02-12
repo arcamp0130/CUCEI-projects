c12364ce46423c195e96fb6c25f83bf17df95107
Arath Campos <mi.familia.55555@gmail.com>
2025-02-11 23:39:57 -0600

    styling and commenting code

--- C/guess number/main.c
+++ C/guess number/main.c
@@ -16,17 +16,20 @@ void clearScreen()
 void printLastHint(int guessTry, unsigned short int randNum)
 {
   printf("3. ");
-  if (guessTry < MAX_RAND_RANGE)
+  if (guessTry <= MAX_RAND_RANGE)
   {
-    if (guessTry > MIN_RAND_RANGE)
+    // if guessTry is in range, then print this hint
+    if (guessTry >= MIN_RAND_RANGE)
       printf("El numero secreto es %s al numero que ingresaste por ultima vez (%d)\n",
              randNum > guessTry ? "mayor" : "menor",
              guessTry);
 
+    // if guessTry is lower than MIN_RAND_RANGE, then alert
     else
       printf("El numero que ingresaste (%d) es menor que el limite inferior permitido (%d)\n",
              guessTry, MIN_RAND_RANGE);
   }
+  // if guessTry is greater than MIN_RAND_RANGE, then alert
   else
     printf("El numero que ingresaste (%d) es mayor que el limite superior permitido (%d)\n",
            guessTry, MAX_RAND_RANGE);
@@ -34,7 +37,7 @@ void printLastHint(int guessTry, unsigned short int randNum)
 
 int main()
 {
-  clearScreen();     // Ensures a clean screen
+  clearScreen();     // Starting with a clean screen
   srand(time(NULL)); // Time seed for random number creation
 
   // Generates a random number between MIN_RAND_RANGE and MAX_RAND_RANGE
@@ -43,14 +46,14 @@ int main()
   int guessTry = 0;
   unsigned int attempts = 0;
 
-  printf("      ================\n");
-  printf("  ========================\n");
-  printf("  ||| Numero aleatorio |||\n");
-  printf("  ========================\n");
-  printf("      ================\n\n");
-  printf("Se obtendra un numero aleatorio entre %d y %d\n",
-         MIN_RAND_RANGE, MAX_RAND_RANGE);
-  printf("el cual debes adivinar, el programa acabara\n");
+  // print 'home page'
+  printf("      ================      \n");
+  printf("  ========================  \n");
+  printf("  ||| Numero aleatorio |||  \n");
+  printf("  ========================  \n");
+  printf("      ================    \n\n");
+  printf("Se obtendra un numero aleatorio en cierto rango,\n");
+  printf("numero el cual debes adivinar. El programa acabara\n");
   printf("cuando consigas tu objetivo.\n");
   printf("Tendras algunas pistas para facilitar tu tarea.\n\n");
   printf("Presiona cualquier tecla para comenzar.\n...");
@@ -62,15 +65,15 @@ int main()
     attempts++;
     printf("Pistas\n");
     printf("---------------\n");
-    // Check if randNum is odd (true) or even (false)
+    // Check if randNum is even (true) or odd (false)
     printf("1. Estoy pensando en un numero %s\n",
            randNum & 1 ? "impar" : "par",
            randNum);
-    printf("2. Mi numero esta entre 1 y 100\n");
+    // print range
+    printf("2. Mi numero esta entre %d y %d\n", MIN_RAND_RANGE, MAX_RAND_RANGE);
+    // print variable hint
     if (attempts > 1)
-    {
       printLastHint(guessTry, randNum);
-    }
 
     printf("---------------\n");
     printf("Adivina! Cual es el numero? \n > ");
@@ -78,6 +81,7 @@ int main()
     isCorrect = guessTry == randNum;
   }
 
+  // Only reachable when user guesses the secret number
   clearScreen();
   printf("    ===============    \n");
   printf("  ===================  \n");
