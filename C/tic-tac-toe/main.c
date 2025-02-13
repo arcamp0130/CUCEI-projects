a99d2627d47ee76d82b1717219d31f5e6c10651d
Arath Campos <113147971+arcamp0130@users.noreply.github.com>
2025-02-13 21:56:28 +0000

    Creating game board and printing preview

--- C/tic-tac-toe/main.c
+++ C/tic-tac-toe/main.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main () {
-  
+void clearScreen()
+{
+  printf("\033[H\033[J");
+  return;
+}
+
+int main()
+{
+  clearScreen();
+
+  int board[3][3];
+
+  // Initialize in -1 to support verification
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      board[i][j] = -1;
+    }
+  }
+
+  // Print game board
+
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      printf("%i", board[i][j]);
+      if (j < 2)
+        printf(" |");
+    }
+    printf("\n");
+    if (i < 2)
+      printf("---|---|---");
+    printf("\n");
+  }
 }
