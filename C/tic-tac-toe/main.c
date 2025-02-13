38c17ef288546e4a9a43e36055bc1302bbd22966
Arath Campos <113147971+arcamp0130@users.noreply.github.com>
2025-02-13 22:22:25 +0000

    printing board with x and y axis. do-while prototype

--- C/tic-tac-toe/main.c
+++ C/tic-tac-toe/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void clearScreen()
 {
@@ -7,34 +8,51 @@ void clearScreen()
   return;
 }
 
-int main()
-{
-  clearScreen();
-
-  int board[3][3];
-
-  // Initialize in -1 to support verification
+void printBoard(unsigned short int board[3][3]) {
+  printf("--------------------\n\n");
+  printf("     1 | 2 | 3   x\n\n");
   for (int i = 0; i < 3; i++)
   {
+    printf(" %i  ", i + 1);
     for (int j = 0; j < 3; j++)
     {
-      board[i][j] = -1;
-    }
-  }
-
-  // Print game board
-
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      printf("%i", board[i][j]);
+      if (board[i][j] != 0) {
+        printf(" %c", board[i][j] == 1 ? "O" : "X");
+      } else {
+        printf("  ");
+      }
       if (j < 2)
         printf(" |");
     }
     printf("\n");
     if (i < 2)
-      printf("---|---|---");
+      printf("    ---|---|---");
     printf("\n");
   }
+  printf(" y\n\n");
+  printf("--------------------\n");
+}
+
+int main()
+{
+  unsigned short int board[3][3], turn = 0;
+  int coordinate[2]; // (x, y). User input
+  bool theresWinner = false;
+  // Initialize in 0 to support verification
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      board[i][j] = 0;
+    }
+  }
+
+  do {
+    clearScreen();
+    printf("Gato!\n");
+    printBoard(board);
+    theresWinner = true;
+  } while (!theresWinner);
+  // Print game board
+  
 }
