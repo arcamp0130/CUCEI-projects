cedb5d4832cb0a504e8c074020adfacd5c013c1b
Arath Campos <mi.familia.55555@gmail.com>
2025-02-18 01:17:01 -0600

    refactor code, printing if invalid input and adding limit of games

--- C/tic-tac-toe/main.c
+++ C/tic-tac-toe/main.c
@@ -37,11 +37,56 @@ void printBoard(unsigned short int board[3][3])
   printf("--------------------\n");
 }
 
+void printWarn(unsigned short int inputStatus)
+{
+  if (inputStatus != 0)
+  {
+    printf("----------\n");
+    printf("## Error ##\n");
+    if (inputStatus == 1)
+      printf("Espacio ocupado");
+    else if (inputStatus == 2)
+      printf("Coordenadas fuera de rango");
+    else
+      printf("Entrada inesperada");
+  }
+
+  printf("\n----------\n");
+  return;
+}
+
+unsigned short int verifyCoordinates(
+    int coords[2],
+    unsigned short int board[3][3])
+{
+  if (coords[0] >= 1 && coords[1] >= 1)
+  {
+    if (coords[0] <= 3 && coords[1] <= 3)
+    {                 // When valid coordinates
+      coords[0] -= 1; // x
+      coords[1] -= 1; // y
+      if (board[coords[1]][coords[0]] == 0)
+        // if not busy space
+        return 0;
+      else // if busy space
+        return 1;
+    }
+    else // if invalid coordinates
+      return 2;
+  }
+  else // if invalid coordinates
+    return 2;
+}
+
 int main()
 {
-  unsigned short int board[3][3], winner = 0;
-  int coord[2];      // (x, y). User input;
-  bool turn = false; // false for 1st, true for 2st
+  unsigned short int
+      board[3][3],      // Game board
+      games = 0,        // Games played
+      inputStatus = 0;  // Game status
+  int coords[2];        // (x, y). User input
+  bool turn = false,    // false for 1st, true for 2st
+       winner = false;  // if there's winner
   // Initialize in 0 to support verification
   for (int i = 0; i < 3; i++)
   {
@@ -57,35 +102,32 @@ int main()
     printf("Gato!\n");
     printBoard(board);
     printf("Jugador %i\n", !turn ? 1 : 2);
+    printWarn(inputStatus);
     printf("Escriba coordenadas para su juego:\n");
     printf(" x > ");
-    scanf("%d", &coord[0]);
+    scanf("%d", &coords[0]);
     printf(" y > ");
-    scanf("%d", &coord[1]);
+    scanf("%d", &coords[1]);
 
     // Check if valid coords
-    if (coord[0] >= 1 && coord[1] >= 1)
+    inputStatus = verifyCoordinates(coords, board);
+    if (inputStatus == 0)
     {
-      if (coord[0] <= 3 && coord[1] <= 3)
-      {
-        coord[0] -= 1; // x
-        coord[1] -= 1; // y
-        if (board[coord[1]][coord[0]] == 0)
-        {
-          board[coord[1]][coord[0]] = !turn ? 1 : 2;
-          turn = !turn;
-        }
-        else
-          printf("Espacio ocupado");
-      }
-      else
-        printf("Coordenadas fuera de rango");
+      board[coords[1]][coords[0]] = !turn ? 1 : 2;
+      turn = !turn;
+      games++;
     }
-    else
-    {
-      printf("Coordenadas fuera de rango");
-    }
-    // winner = 1;
-  } while (winner == 0);
-  // Print game board
+
+  } while (!winner && games < 9);
+  clearScreen();
+  printBoard(board);
+  if (!winner)
+  {
+    printf("Empate!\n");
+  }
+  else
+  {
+    printf("Gano el jugador %i\n", !turn ? 1 : 2);
+  }
+  return 0;
 }
